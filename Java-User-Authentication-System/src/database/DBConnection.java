2117fa060d222f0fc24733d051a81e89456621bc
SMD ASIF <smdasif1804@gmail.com>
2026-03-09 15:26:40 +0530

    Updated project: added MySQL database integration, DAO layer, login validation, and welcome page UI

--- Java-User-Authentication-System/src/database/DBConnection.java
+++ Java-User-Authentication-System/src/database/DBConnection.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 public class DBConnection {
       static String url ="jdbc:mysql://localhost:3306/user_system";
       static String user = "root";
-      static String password="Asifsmd@1975";
+      static String password="your_password";
       public static Connection getConnection() throws Exception {
             Class.forName("com.mysql.cj.jdbc.Driver");
             return DriverManager.getConnection(url,user,password);
