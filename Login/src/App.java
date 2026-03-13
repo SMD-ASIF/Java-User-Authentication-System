8347a09bdd1468b3c7091547d57983d75711316d
Shaik Mahammad Asif <smdasif1804@gmail.com>
2026-03-13 23:59:43 +0530

    Update file path for user data serialization

--- Login/src/App.java
+++ Login/src/App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
     
-        String filename = "C:\\Users\\smdas\\OneDrive\\Desktop\\user_data.ser";
+        String filename = "user_data.ser";
         UsernameandPasswords userpasswords=new UsernameandPasswords(filename);
         new createaccount(userpasswords);
         
